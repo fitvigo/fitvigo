fbfae1b6ab2f2b4bc561bcd68b6eb1776f345868
Sergey Karinsky <reactlife.js@gmail.com>
2016-07-17 21:26:01 +0700

    feat(all): header and navigation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,16 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import 'fullpage.js';
 
 $(() => {
 	svg4everybody();
+
+	const anchors = ['intro', 'exchange', 'create', 'investments', 'account', 'trade', 'footer'];
+
+	$('#fullpage').fullpage({
+		scrollbar: false,
+		navigation: true,
+		navigationPosition: 'index__fullpage-nav',
+		anchors
+	});
 });
