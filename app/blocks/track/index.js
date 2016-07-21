181a98818b8a10ad95396fe5cc0b83a6b8ba5cf9
Sergey Karinsky <reactlife.js@gmail.com>
2016-07-21 17:43:37 +0700

    fix errors

--- app/blocks/track/index.js
+++ app/blocks/track/index.js
@@ -11,26 +11,6 @@ export default function (direction, index, speed) {
 		}, speed);
 	}
 
-	if (direction === 'up' && index === 5) {
-		$('.track__desc_second').css({display: 'none'});
-		$('.track__desc_first').css({display: 'block'});
-	}
-
-	if (direction === 'down' && index === 4) {
-		$('.track__desc_first').css({display: 'none'});
-		$('.track__desc_second').css({display: 'block'});
-	}
-
-	if (direction === 'up' && index === 6) {
-		$('.track__desc_second').css({display: 'block'});
-		$('.track__desc_third').css({display: 'none'});
-	}
-
-	if (direction === 'down' && index === 5) {
-		$('.track__desc_second').css({display: 'none'});
-		$('.track__desc_third').css({display: 'block'});
-	}
-
 	if ((direction === 'down' && index === 5) || index === 6 || index === 7) {
 		const delayInc = 300;
 		let delay = delayInc;
