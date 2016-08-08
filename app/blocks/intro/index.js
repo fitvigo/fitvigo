b6c0b14ea4b22dbb2f9f7c381ce1801d03ac1b13
Sergey Karinsky <reactlife.js@gmail.com>
2016-08-08 15:49:38 +0700

    fix(intro): pause video

--- app/blocks/intro/index.js
+++ app/blocks/intro/index.js
@@ -1,29 +1,30 @@
-import $ from 'jquery';
-import 'jquery.scrollto';
-
 const block = 'intro';
 const classname = '.' + block;
 
 const video = document.getElementById('avi-video');
 const video2 = document.getElementById('avi-video2');
 
+video.pause();
+video2.pause();
 setTimeout(function () {      
 	video.play();
 	video2.play();
-}, 10);
+}, 100);
 
 export default function (direction, index, speed) {
 	if ((direction === 'down' && index === 2) || 
 			(direction === 'up' && index === 4)) {
+		video2.pause();
 		setTimeout(function () {      
 			video2.play();
-		}, 10);
+		}, 100);
 	}
 
 	if ((direction === 'down' && index === 1) || 
 			(direction === 'up' && index === 3)) {
+		video.pause();
 		setTimeout(function () {      
 			video.play();
-		}, 10);
+		}, 100);
 	}
 }
