55272fa151869e55cca044a505963d6ad4c284f2
Sergey Karinsky <reactlife.js@gmail.com>
2016-08-10 19:02:56 +0700

    fix(all): mobile bugs

--- app/blocks/intro/index.js
+++ app/blocks/intro/index.js
@@ -1,22 +1,24 @@
 const video = document.getElementById('avi-video');
 const video2 = document.getElementById('avi-video2');
 
-video.pause();
-video2.pause();
+if (video && video2) {
+	video.pause();
+	video2.pause();
 
-setTimeout(function() {
-	video.play();
-	video2.play();
-}, 100);
-
-video.onpause = function() {
 	setTimeout(function() {
 		video.play();
-	}, 100);
-};
-
-video2.onpause = function() {
-	setTimeout(function() {
 		video2.play();
 	}, 100);
-};
+
+	video.onpause = function() {
+		setTimeout(function() {
+			video.play();
+		}, 100);
+	};
+
+	video2.onpause = function() {
+		setTimeout(function() {
+			video2.play();
+		}, 100);
+	};
+}
