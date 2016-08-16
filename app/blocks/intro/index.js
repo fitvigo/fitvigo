24acc5a1897171fb0979b8ea2df66b9c1e6d869b
Sergey Karinsky <reactlife.js@gmail.com>
2016-08-17 01:46:42 +0700

    fix(intro): avi as image

--- app/blocks/intro/index.js
+++ app/blocks/intro/index.js
@@ -1,24 +1,15 @@
 const video = document.getElementById('avi-video');
-const video2 = document.getElementById('avi-video2');
 
-if (video && video2) {
+if (video) {
 	video.pause();
-	video2.pause();
 
 	setTimeout(function() {
 		video.play();
-		video2.play();
 	}, 100);
 
 	video.onpause = function() {
 		setTimeout(function() {
 			video.play();
-		}, 100);
-	};
-
-	video2.onpause = function() {
-		setTimeout(function() {
-			video2.play();
 		}, 100);
 	};
 }
